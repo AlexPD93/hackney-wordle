00d2d9d4445e9972524970b41af1fb02ceef379a
Alex Perez-Davies <alexperezdavies@gmail.com>
2024-04-27 17:50:40 +0100

    add logic to check guess and highlight tiles and keys with correct colour

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let guessesRemaining = totalRows;
 let currentGuess = [];
 let currentTile = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
-
+console.log(rightGuessString);
 function initBoard() {
   let board = document.getElementById("game-board");
 
@@ -30,6 +30,10 @@ document.addEventListener("keyup", (e) => {
     removeLetterFromBoard();
   }
 
+  if (pressedKey === "Enter") {
+    checkGuess();
+  }
+
   let letterMatched = pressedKey.match(/[a-z]/gi);
 
   if (!letterMatched || letterMatched.length > 1) {
@@ -47,6 +51,7 @@ function addLetterToBoard(pressedKey) {
 
   let currentRow =
     document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+
   let box = currentRow.children[currentTile];
 
   box.textContent = pressedKey;
@@ -66,4 +71,71 @@ function removeLetterFromBoard() {
   currentTile -= 1;
 }
 
+function checkGuess() {
+  let currentRow =
+    document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+  let guessString = "";
+
+  guessString = currentGuess.join("");
+
+  if (guessString.length < 5) {
+    console.log("not enough letters");
+    return;
+  }
+
+  if (!WORDS.includes(guessString)) {
+    console.log("word not in list!");
+    return;
+  }
+
+  for (let i = 0; i < currentGuess.length; i++) {
+    let box = currentRow.children[i];
+
+    let letter = currentGuess[i];
+
+    let letterPosition = rightGuessString.indexOf(currentGuess[i]);
+
+    if (letterPosition === -1) {
+      let grey = "#787C7E";
+      fillTile(box, grey);
+      fillKeyboard(letter, grey);
+    } else {
+      if (currentGuess[i] === rightGuessString[i]) {
+        let green = "#6AAA64";
+        fillTile(box, green);
+        fillKeyboard(letter, green);
+      } else {
+        let yellow = "#C9B458";
+        fillTile(box, yellow);
+        fillKeyboard(letter, yellow);
+      }
+    }
+  }
+
+  if (guessString === rightGuessString) {
+    console.log("You won!");
+    guessesRemaining = 0;
+  }
+
+  guessesRemaining -= 1;
+  currentGuess = [];
+  currentTile = 0;
+}
+
+function fillTile(box, letterColour) {
+  box.style.backgroundColor = letterColour;
+}
+
+function fillKeyboard(letter, letterColour) {
+  let keys = document.getElementsByClassName("keyboard-button");
+
+  Array.from(keys).forEach((key) => {
+    if (letter === key.textContent) {
+      key.style.backgroundColor = letterColour;
+    }
+  });
+}
+
 initBoard();
+
+// If guess has double letter but correct word only has one only highlight one letter
